a3c91a45cd00b8791f639f7aa3469c6049350b56
Elena Soloveva <seaver@yandex.ru>
2023-02-01 13:49:59 +0200

    Feat: add pm2 deploy for frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./dist/app.js"
+  }],
+   deploy: {
+      production: {
+        user: DEPLOY_USER,
+        host: DEPLOY_HOST,
+        ref: DEPLOY_REF,
+        repo: 'git@github.com:ElenaSolov/web-plus-pm2-deploy.git',
+        path: DEPLOY_PATH,
+        'post-deploy': 'npm i && npm run build',
+      },
+    },
+}
