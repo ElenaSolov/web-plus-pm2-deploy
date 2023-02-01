e656a3013baa1357749437c5a3d0b9ace18dbca5
Elena Soloveva <seaver@yandex.ru>
2023-02-01 08:00:59 +0200

    Fix: change backend url to https

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://api.acrosstheworld.nomoredomains.rocks');
+  const api = new Api('https://api.acrosstheworld.nomoredomains.rocks');
 
   export default api;
