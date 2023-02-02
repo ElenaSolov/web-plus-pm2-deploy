3b7916191608ef23a96be8cd3fc7c54084972199
Elena Soloveva <seaver@yandex.ru>
2023-02-02 10:11:47 +0200

    Fix: fix path to plugin.js

--- backend/src/controllers/plugins.ts
+++ backend/src/controllers/plugins.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
+import path from 'path';
 
 const sendStaticJs = (req: Request, res: Response) => {
   res
-    .sendFile('public/plugin.js');
+    .sendFile('public/plugin.js', { root: path.join(__dirname, '../../public') });
 };
 
 export default sendStaticJs;
